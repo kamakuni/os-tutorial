dd933224fbf4a2547a04c2a05287bbf44136c280
sugawara kensuke <superradio86@gmail.com>
2021-08-20 23:37:55 +0900

    add codes for attempt 2

--- os-tutorial/02-bootsector-print/03-bootsector-memory/boot_sect_memory.asm
+++ os-tutorial/02-bootsector-print/03-bootsector-memory/boot_sect_memory.asm
@@ -7,3 +7,12 @@ mov al, "1"
 int 0x10
 mov al, the_secret
 int 0x10
+
+; attempt 2¥
+; It tries to print the memory address of 'the_secret' which is the correct approach.
+; However, BIOS places our bootsector binary at address 0x7c00
+; so we need to add that padding beforehand. We'll do that in attempt 3
+mov al, "2"
+int 0x10
+mov al, [the_secret]
+int 0x10
