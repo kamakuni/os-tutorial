694b52623ef16bf22cd0e027c0b8ac58e9e5531b
sugawara kensuke <superradio86@gmail.com>
2021-08-21 22:30:45 +0900

    add codes for attempt 3

--- os-tutorial/02-bootsector-print/03-bootsector-memory/boot_sect_memory.asm
+++ os-tutorial/02-bootsector-print/03-bootsector-memory/boot_sect_memory.asm
@@ -8,7 +8,7 @@ int 0x10
 mov al, the_secret
 int 0x10
 
-; attempt 2¥
+; attempt 2
 ; It tries to print the memory address of 'the_secret' which is the correct approach.
 ; However, BIOS places our bootsector binary at address 0x7c00
 ; so we need to add that padding beforehand. We'll do that in attempt 3
@@ -16,3 +16,16 @@ mov al, "2"
 int 0x10
 mov al, [the_secret]
 int 0x10
+
+; attempt 3
+; Add the BIOS starting offset 0x7c00 to the memory address of the X
+; and then dereference the contents of that pointer.
+; We need the help of a different register 'bx' because 'mov al, [ax]' is illegal.
+; A register can't be used as source and destination for the same command.
+mov al, "3"
+int 0x10
+mov bx, the_secret
+add bx, 0x7c00
+mov al, [bx]
+int 0x10
+
