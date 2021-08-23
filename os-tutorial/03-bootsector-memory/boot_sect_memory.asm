a653e54cd61ac893fd0caa61baf263ecfb1849de
sugawara kensuke <superradio86@gmail.com>
2021-08-23 20:35:14 +0900

    Add infinite loop

--- os-tutorial/03-bootsector-memory/boot_sect_memory.asm
+++ os-tutorial/03-bootsector-memory/boot_sect_memory.asm
@@ -37,3 +37,14 @@ mov al, "4"
 int 0x10
 mov al, [0x7c2d]
 int 0x10
+
+jmp $ ; infinite loop
+
+the_secret:
+    ; ASCII code 0x58 ('X') is stored just before the zore-padding.
+    ; On this code that is at byte 0x2d (check it out using 'xxx file.bin')
+    db "X"
+
+; zero padding and magic bios number
+times 510-($-$$) db 0
+dw 0xaa55
