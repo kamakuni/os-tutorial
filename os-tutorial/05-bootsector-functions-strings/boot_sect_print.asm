887ba896be6728721b4e913d0643977bac8e3991
sugawara kensuke <superradio86@gmail.com>
2021-08-31 23:34:26 +0900

    Update boot_sect_print.asm

--- os-tutorial/05-bootsector-functions-strings/boot_sect_print.asm
+++ os-tutorial/05-bootsector-functions-strings/boot_sect_print.asm
@@ -9,4 +9,26 @@ start:
     mov al, [bx]
     cmp al, 0
     je done
-    
+
+    ; the part where we print with the BIOS help
+    mov ah, 0x0e
+    int 0x10 ; 'al' already contains char
+
+    ; increment pointer and do next loop
+    add bx, 1
+    jmp start
+
+done:
+    popa
+    ret
+
+print_nl:
+    pusha
+
+    mov al, 0x0e
+    mov al, 0x0a ; newline char
+    int 0x10
+    mov al, 0x0d ; carriage return
+
+    popa
+    ret
