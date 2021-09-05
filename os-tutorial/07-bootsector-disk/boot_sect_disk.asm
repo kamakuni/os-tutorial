2c1628518fc73c2fa84c6a8c06882f14f60c4144
sugawara kensuke <superradio86@gmail.com>
2021-09-05 23:53:18 +0900

    Add boot_sect_disk.asm

---
+++ os-tutorial/07-bootsector-disk/boot_sect_disk.asm
@@ -0,0 +1,6 @@
+; load 'dh' sectors from drive 'dl' into ES:BX
+disk_load:
+    pusha
+    ; reading from disk requires setting specific values in all registers
+    ; so we will overwrite our input parameters from 'dx'. Let's save it
+    ; to the stack for later use
