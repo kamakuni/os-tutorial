30fcf0c40157044ff8cc1bb83a6d24ce19bf79dd
sugawara kensuke <superradio86@gmail.com>
2021-09-06 23:13:26 +0900

    Update boot_sect_disk.asm

--- os-tutorial/07-bootsector-disk/boot_sect_disk.asm
+++ os-tutorial/07-bootsector-disk/boot_sect_disk.asm
@@ -3,4 +3,10 @@ disk_load:
     pusha
     ; reading from disk requires setting specific values in all registers
     ; so we will overwrite our input parameters from 'dx'. Let's save it
-    ; to the stack for later use
+    ; to the stack for later use.
+    push dx
+
+    mov al, 0x02 ; ah <- int 0x13 function. 0x02 = 'read'
+    mov al, dh   ; al <- number of sectors to read (0x01 .. 0x80)
+    mov cl, 0x02 ; cl <- sector (0x01 .. 0x11)
+                 ; 0x01 is our boot sector, 0x02 is the first 'available' sector
