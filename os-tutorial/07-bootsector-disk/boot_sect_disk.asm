deff2609cb14f1fff15e1d8d033979b109fd92a5
sugawara kensuke <superradio86@gmail.com>
2021-09-12 23:59:49 +0900

    Update boot_sect_disk.asm

--- os-tutorial/07-bootsector-disk/boot_sect_disk.asm
+++ os-tutorial/07-bootsector-disk/boot_sect_disk.asm
@@ -25,3 +25,11 @@ disk_load:
     jne sectors_error
     popa
     ret
+
+disk_error:
+    mov bx, DISK_ERROR
+    call print
+    call print_nl
+    mov dh, ah ; ah = error code, dl = disk drive that dropped the error
+    call print_hex ; check out the code at http://stanislavs.org/helppc/int_13-1.html
+    jmp disk_loop
