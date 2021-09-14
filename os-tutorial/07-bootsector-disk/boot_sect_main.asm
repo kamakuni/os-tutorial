e63bf0956b02aba6c08ab4ddb25d79d710a1ec4c
sugawara kensuke <superradio86@gmail.com>
2021-09-14 20:46:32 +0900

    Add boot_sect_main.asm

---
+++ os-tutorial/07-bootsector-disk/boot_sect_main.asm
@@ -0,0 +1,27 @@
+[org 0x7c00]
+    mov bp, 0x8000 ; set the stack safely away from us
+    mov sp, bp
+
+    mov bx, 0x9000 ; es:bx = 0x0000:0x9000 = 0x9000
+    mov dh, 2 ; read 2 sectors
+    ; the bios sets 'dl' for our boot disk number
+    ; if you have trouble, use the '-fda' flag: 'qemu -fda file.bin'
+    call disk_load
+
+    mov dx, [0x9000] ; retrieve the first loaded word, 0xdata
+    call print_hex
+
+    call print_nl
+
+    mov dx, [0x9000 + 512] ; first word from second loaded sector, 0xface
+    call print_hex
+
+    jmp $
+
+%include "../05-bootsector-functons-strings/boot_sect_print.asm"
+%include "../05-bootsector-functions-strings/boot_sect_print_hex.asm"
+%include "boot_sect_disk.asm"
+
+; Maginc number
+times 510 - ($-$$) db 0
+dw 0xaa55
