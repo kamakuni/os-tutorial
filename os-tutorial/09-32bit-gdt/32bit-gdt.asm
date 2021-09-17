2ec8e08c7d2c4d3f14e053a035fd15aba9b9824f
sugawara kensuke <superradio86@gmail.com>
2021-09-17 23:22:37 +0900

    Update 32bit-gdt.asm

--- os-tutorial/09-32bit-gdt/32bit-gdt.asm
+++ os-tutorial/09-32bit-gdt/32bit-gdt.asm
@@ -13,3 +13,23 @@ gdt_code:
     db 11001111b ; flags (4 bits) + segment length, bits 16-19
     db 0x0       ; segment base, bits 24-31
 
+; GDT for data segment, base and length identical to code segment
+; some flags changed, again, refer to os-dev.pdf
+gdt_data:
+    dw 0xffff
+    dw 0x0
+    dw 0x0
+    db 10011010b
+    db 11001111b
+    db 0x0
+
+gdt_end:
+
+; GDT descriptor
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1 ; size (16 bit), always one less of its true size
+    dd gdt_start ; address (32 bit)
+
+; define some constants for later use
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
