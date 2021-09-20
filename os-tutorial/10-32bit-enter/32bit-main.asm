f1c1636e38338b52bb463bf9f1838d0237485956
sugawara kensuke <superradio86@gmail.com>
2021-09-20 22:10:03 +0900

    Update 32bit-main.asm

--- os-tutorial/10-32bit-enter/32bit-main.asm
+++ os-tutorial/10-32bit-enter/32bit-main.asm
@@ -7,3 +7,21 @@
 
     call switch_to_pm
     jmp $ ; this will actually never be executed
+
+%include "../05-bootsector-functions-strings/boot_sect_print.asm"
+%include "../09-32bit-gdt/32bit-gdt.asm"
+%include "../08-32bit-print/32bit-print.asm"
+%include "32bit-switch.asm"
+
+[bits 32]
+BEGIN_PIN: ; after the switch we will get here
+    mov ebx, MSG_PROT_MODE
+    call print_string_pm ; Note that this will be written at the top left corner
+    jmp $
+
+MSG_REAL_MODE db "Started on 16-bit real mode", 0
+MSG_PROT_MODE db "Started on 32-bit protected mode", 0
+
+; bootsector
+times 510-($-$$) db 0
+dw 0xaa55
