3382ba665346069ec8b2d0017e88d9d8a325cb02
sugawara kensuke <superradio86@gmail.com>
2021-09-28 23:54:58 +0900

    Update bootsect.asm

--- os-tutorial/13-kernel-barebones/bootsect.asm
+++ os-tutorial/13-kernel-barebones/bootsect.asm
@@ -13,4 +13,22 @@ KERNEL_OFFSET equ 0x1000 ; The same one we used when linking the kernel
     call switch_to_pm ; disable interrupts, load GDT, etc. Finally jumps to 'BEGIN_PM'
     jmp $ ; Never executed
 
-    
+%include "../05-bootsector-functions-strings/boot_sect_print.asm"
+%include "../05-bootsector-functions-strings/boot_sect_print_hex.asm"
+%include "../07-bootsector-disk/boot_sect_disk.asm"
+%include "../09-32bit-gdt/32bit-gdt.asm"
+%include "../08-32bit-print/32bit-print.asm"
+%include "../10-32bit-enter/32bit-switch.asm"
+
+[bits 16]
+load_kernel:
+    mov bx, MSG_LOAD_KERNEL
+    call print
+    call print_nl
+
+    mov bx, KERNEL_OFFSET ; Read from disk and store in 0x1000
+    mov dh, 2
+    mov dl, [BOOT_DRIVE]
+    call disk_load
+    ret
+
