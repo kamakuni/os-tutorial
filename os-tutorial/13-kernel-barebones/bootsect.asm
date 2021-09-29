c78566eed34296da9e6359d03b4c4cd813b82e22
sugawara kensuke <superradio86@gmail.com>
2021-09-29 19:20:49 +0900

    Update bootsect.asm

--- os-tutorial/13-kernel-barebones/bootsect.asm
+++ os-tutorial/13-kernel-barebones/bootsect.asm
@@ -32,3 +32,18 @@ load_kernel:
     call disk_load
     ret
 
+[bits 32]
+BEGIN_PIN:
+    mov ebx, MSG_PROT_MODE
+    call print_string_pm
+    call KERNEL_OFFSET ; Give control to the kernel
+    jmp $ ; Stay here when the kernel returns control to us (if ever)
+
+BOOT_DRIVE db 0 ; It is a good idea to store it in memory because 'dl' may get overwritten
+MSG_REAL_MODE db "Started in 16-bit Real Mode", 0
+MSG_PROT_MODE db "Landed in 32-bit Protected Mode", 0
+MSG_LOAD_KERNEL db "Loading kernel into memory", 0
+
+; padding
+times 510 - ($-$$) db 0
+dw 0xaa55
