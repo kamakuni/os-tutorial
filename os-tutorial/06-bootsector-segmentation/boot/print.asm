bc0784d97aff22ada75ea0ceef9c001ddf6e7e64
sugawara kensuke <superradio86@gmail.com>
2021-10-09 23:32:52 +0900

    Add print.asm

---
+++ os-tutorial/06-bootsector-segmentation/boot/print.asm
@@ -0,0 +1,34 @@
+print:
+    pusha
+
+; keep this in mind:
+; while (strings[i] != 0) { print string[i] i++ }
+
+; the comparison for string end (null byte)
+start:
+    mov al, [bx] ; 'bx' is the base address for the string
+    comp al, 0
+    je done
+
+    ; this part where we print with the BIOS help
+    mov ah, 0x0e
+    int 0x10 ; 'al' already contains the char
+
+    ; increment pointer and do next loop
+    add bx, 1
+    jmp start
+
+done:
+    popa
+    ret
+
+print_nl:
+    pusha
+
+    mov ah, 0x0e
+    mov al, 0x0a ; newline char
+    int 0x10
+    mov al, 0x0d ; carriage return
+
+    popa
+    ret
