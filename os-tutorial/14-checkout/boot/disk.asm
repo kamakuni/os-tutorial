7036c7db0276d1cf1d735ff3b7ef178e15de3a5f
sugawara kensuke <superradio86@gmail.com>
2021-10-13 20:54:51 +0900

    Add disk.asm

---
+++ os-tutorial/14-checkout/boot/disk.asm
@@ -0,0 +1,27 @@
+; load 'dh' sectors from drive 'dl' into ES:BX
+;disk_load:
+;    pusha
+    ; reading from disk requires setting specifc values in all registers
+    ; so we will overwrite our input parameters from 'dx'. Let's save it
+    ; to the stack for later use.
+;    push dx
+
+;    mov ah, 0x02 ; 
+
+disk_error:
+    mov bx, DISK_ERROR
+    call print
+    call print_nl
+    mov dh, ah ; ah = error code, dl = disk drive that dropped the error
+    call print_hex ; check out the code at http://stanislavs.org/helppc/int_13-1.html
+    jmp disk_loop
+
+sectors_error:
+    mov bx, SECTORS_ERROR
+    call print
+
+disk_loop:
+    jmp $
+
+DISK_ERROR: db "Disk read error", 0
+SECTORS_ERROR: db "Incorrect numebr of sectors read", 0
