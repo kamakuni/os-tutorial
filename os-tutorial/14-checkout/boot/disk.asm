127dd0e2ff7377e1a89d0fedc2cece00fc13a9ac
sugawara kensuke <superradio86@gmail.com>
2021-10-14 20:38:55 +0900

    Update disk.asm

--- os-tutorial/14-checkout/boot/disk.asm
+++ os-tutorial/14-checkout/boot/disk.asm
@@ -1,12 +1,26 @@
 ; load 'dh' sectors from drive 'dl' into ES:BX
-;disk_load:
-;    pusha
+disk_load:
+    pusha
     ; reading from disk requires setting specifc values in all registers
     ; so we will overwrite our input parameters from 'dx'. Let's save it
     ; to the stack for later use.
-;    push dx
+    push dx
 
-;    mov ah, 0x02 ; 
+    mov ah, 0x02 ; 
+    mov al, dh
+    mov cl, 0x02
+
+    mov ch, 0x00
+    mov dh, 0x00
+
+    int 0x13
+    jc disk_error
+
+    pop dx
+    cmp al, dh
+    jne sectors_error
+    popa
+    ret
 
 disk_error:
     mov bx, DISK_ERROR
