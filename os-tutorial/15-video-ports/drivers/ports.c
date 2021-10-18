441e5e1b50087b59887a524af7e3eb78b80279f0
sugawara kensuke <superradio86@gmail.com>
2021-10-18 22:30:42 +0900

    Implement port_byte_in function

---
+++ os-tutorial/15-video-ports/drivers/ports.c
@@ -0,0 +1,15 @@
+/**
+ * Read a byte from the specified port
+ */
+unsigned char port_byte_in (unsigned short port) {
+    unsigned char result;
+    /* Inline assembler syntax
+     * !! Notice how the source and destination registers are switched from NASM !!
+     *
+     * '"=a" (result)'; set '=' the C variable '(result)' to the value of register e'a'x
+     * '"d" (port)': mapo the C variable '(port)' int e'd'x register
+     * Inputs and outputs are separated by colons
+     */
+    __asm__("in %%dx, %%al" : "=a" (result) : "d" (port));
+    return result;
+}
