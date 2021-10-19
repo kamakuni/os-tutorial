d53486d30cd30901541bb2b00648674079e82032
sugawara kensuke <superradio86@gmail.com>
2021-10-19 13:33:34 +0900

    Implement port_byte_out function

--- os-tutorial/15-video-ports/drivers/ports.c
+++ os-tutorial/15-video-ports/drivers/ports.c
@@ -13,3 +13,12 @@ unsigned char port_byte_in (unsigned short port) {
     __asm__("in %%dx, %%al" : "=a" (result) : "d" (port));
     return result;
 }
+
+void port_byte_out (unsigned short port, unsigned char data) {
+    /* Notice how here both registers are mapped to C variables and
+     * nothing is returned, thus, no equals '=' in the asm syntax
+     * However we see a come since there are two variables in the input area
+     * and none in the 'return' area
+     */
+    __asm__("out %%al, %%dx" : : "a" (data), "d" (port));
+}
