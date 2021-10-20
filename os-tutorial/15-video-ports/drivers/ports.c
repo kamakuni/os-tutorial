564d116e8979c546788f495d4bf1e3dc561c9419
sugawara kensuke <superradio86@gmail.com>
2021-10-20 23:55:18 +0900

    Implement port_word_in function

--- os-tutorial/15-video-ports/drivers/ports.c
+++ os-tutorial/15-video-ports/drivers/ports.c
@@ -22,3 +22,9 @@ void port_byte_out (unsigned short port, unsigned char data) {
      */
     __asm__("out %%al, %%dx" : : "a" (data), "d" (port));
 }
+
+unsigned short port_word_in (unsigned short port) {
+    unsigned short result;
+    __asm__("in %%dx, %%ax" : "=a" (result) : "d" (port));
+    return result;
+}
