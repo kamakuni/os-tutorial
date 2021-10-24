d33c164a3dcc1bc4ea5b70a4cd012824bbaa2742
sugawara kensuke <superradio86@gmail.com>
2021-10-24 23:59:25 +0900

    Typo

--- os-tutorial/09-32bit-gdt/32bit-gdt.asm
+++ os-tutorial/09-32bit-gdt/32bit-gdt.asm
@@ -3,12 +3,12 @@ gdt_start: ; don't remove the labels, they're needed to compute sizes and jumps
     dd 0x0 ; 4 byte
     dd 0x0 ; 4 byte
 
-; GDT for code segment, bse = 0x00000000, length = 0xfffff
+; GDT for code segment, base = 0x00000000, length = 0xfffff
 ; for flags, refer to os-dev.pdf document, page 36
 gdt_code:
     dw 0xffff    ; segment length, bits 0-15
     dw 0x0       ; segment base, bits 0-15
-    dw 0x0       ; segment base, bits 16-23
+    db 0x0       ; segment base, bits 16-23
     db 10011010b ; flags (8 bits)
     db 11001111b ; flags (4 bits) + segment length, bits 16-19
     db 0x0       ; segment base, bits 24-31
@@ -18,7 +18,7 @@ gdt_code:
 gdt_data:
     dw 0xffff
     dw 0x0
-    dw 0x0
+    db 0x0
     db 10011010b
     db 11001111b
     db 0x0
