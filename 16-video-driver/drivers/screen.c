8e9b6ac9eb89c844fb13919f16b273d895861287
sugawara kensuke <superradio86@gmail.com>
2021-10-31 22:11:38 +0900

    Add set_cursor_offset and clear_screen function

--- 16-video-driver/drivers/screen.c
+++ 16-video-driver/drivers/screen.c
@@ -92,3 +92,24 @@ int get_cursor_offset() {
     offset += port_byte_in(REG_SCREEN_DATA);
     return offset * 2; /* Position * size of character cell */
 }
+
+void set_cursor_offset() {
+    /* Similar to get_cursor_offset, but instead of reading we write data */
+    offset /= 2;
+    port_byte_out(REG_SCREEN_CTRL, 14);
+    port_byte_out(REG_SCREEN_DATA, (unsigned char)(offset >> 8));
+    port_byte_out(REG_SCREEN_CTRL, 15);
+    port_byte_out(REG_SCREEN_DATA, (unsigned char)(offset & 0xff));
+}
+
+void clear_screen() {
+    int screen_size = MAX_COLS * MAX_ROWS;
+    int i;
+    char *screen = VIDEO_ADDRESS;
+
+    for (i = 0; i < screen_size; i++) {
+        screen[i*2] = ' ';
+        screen[i*2+1] = WHITE_ON_BLACK;
+    }
+    set_cursor_offset(get_offset(0,0));
+}
