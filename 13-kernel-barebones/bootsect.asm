b562b123d11e1f0f6bc767ce3fa69a2e8e4ed164
sugawara kensuke <superradio86@gmail.com>
2021-11-07 23:57:01 +0900

    Fix typo

--- 13-kernel-barebones/bootsect.asm
+++ 13-kernel-barebones/bootsect.asm
@@ -1,7 +1,7 @@
 [org 0x7c00]
 KERNEL_OFFSET equ 0x1000 ; The same one we used when linking the kernel
 
-    mov [BOOT_DRIVE], dl ; Remember that the BIOS sets us the boot drive in 'dl on boot
+    mov [BOOT_DRIVE], dl ; Remember that the BIOS sets us the boot drive in 'dl' on boot
     mov bp, 0x9000
     mov sp, bp
 
@@ -33,11 +33,12 @@ load_kernel:
     ret
 
 [bits 32]
-BEGIN_PIN:
+BEGIN_PM:
     mov ebx, MSG_PROT_MODE
     call print_string_pm
     call KERNEL_OFFSET ; Give control to the kernel
     jmp $ ; Stay here when the kernel returns control to us (if ever)
+
 
 BOOT_DRIVE db 0 ; It is a good idea to store it in memory because 'dl' may get overwritten
 MSG_REAL_MODE db "Started in 16-bit Real Mode", 0
