87e090754c11d0565cb6bfe4f86a9939031864c1
sugawara kensuke <superradio86@gmail.com>
2021-11-19 23:07:50 +0900

    Update interrupt.asm

--- 18-interrupts/cpu/interrupt.asm
+++ 18-interrupts/cpu/interrupt.asm
@@ -12,3 +12,17 @@ isr_common_stub:
         mov es, ax
         mov fs, ax
         mov gs, ax
+    
+    ; 2. Call C handler
+        call isr_handler
+
+    ; 3. Restore state
+        pop eax
+        mov ds, ax
+        mov es, ax
+        mov fs, ax
+        mov gs, ax
+        popa
+        add esp, 8 ; Cleans up the pushed error code and pushed ISR number
+        sti
+        iret ; pops 5 things at once: CS, EIP, EFLAGS, SS, and ESP
