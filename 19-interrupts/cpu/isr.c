3367893e0ba84bd45a30a04ab3d4a94e7a51b428
sugawara kensuke <superradio86@gmail.com>
2021-11-22 23:22:05 +0900

    Add register_interrup_handler and irq_handler functions

--- 19-interrupts/cpu/isr.c
+++ 19-interrupts/cpu/isr.c
@@ -124,3 +124,24 @@ void isr_handler(registers_t r)
     kprint(exception_messages[r.int_no]);
     kprint("\n");
 }
+
+void register_interrupt_handler(u8 n, isr_t handler)
+{
+    interrupt_handlers[n] = handler;
+}
+
+void irq_handler(registers_t r)
+{
+    /* After every interrupt we need to send an EOOOI to the PICs
+     * or they will not send another interrupt again */
+    if (r.int_no >= 40)
+        port_byte_out(0xA0, 0x20); /* slave */
+    port_byte_out(0x20, 0x20);     /* master */
+
+    /* Handle a interrupt in a more modular way */
+    if (interrupt_handler[r.int_no] != 0)
+    {
+        isr_h handler = interrupt_handlers[r.int_no];
+        handler(r);
+    }
+}
