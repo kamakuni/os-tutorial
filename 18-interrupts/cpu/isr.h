8d0fac3995dc89e9518a3c217371bcc3bf067457
sugawara kensuke <superradio86@gmail.com>
2021-11-23 20:35:59 +0900

    Update isr.h

--- 18-interrupts/cpu/isr.h
+++ 18-interrupts/cpu/isr.h
@@ -37,15 +37,36 @@ extern void isr29();
 extern void isr30();
 extern void isr31();
 
+#define IRQ0 32
+#define IRQ1 33
+#define IRQ2 34
+#define IRQ3 35
+#define IRQ4 36
+#define IRQ5 37
+#define IRQ6 38
+#define IRQ7 39
+#define IRQ8 40
+#define IRQ9 41
+#define IRQ10 42
+#define IRQ11 43
+#define IRQ12 44
+#define IRQ13 45
+#define IRQ14 46
+#define IRQ15 47
+
 /* Struct which aggregates many registers */
-typedef struct {
-    u32 ds; /* Data segment selector */
-    u32 edi, esi, ebp, esp, ebx, ecx,eax; /* Pushed by pusha. */
-    u32 int_no, err_code; /* Interrupts number and error code (if applicable) */
-    u32 epi, cs, eflags, useresp, ss; /* Pushed by the processor automatically */
+typedef struct
+{
+    u32 ds;                                /* Data segment selector */
+    u32 edi, esi, ebp, esp, ebx, ecx, eax; /* Pushed by pusha. */
+    u32 int_no, err_code;                  /* Interrupts number and error code (if applicable) */
+    u32 epi, cs, eflags, useresp, ss;      /* Pushed by the processor automatically */
 } registers_t;
 
 void isr_install();
 void isr_handler(registers_t r);
+
+typedef void (*isr_t)(registers_t);
+void register_interrupt_handler(u8 n, isr_h handler);
 
 #endif
