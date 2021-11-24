7b18ccb8be1ee7c3b1e460a0fad42ab10714ea65
sugawara kensuke <superradio86@gmail.com>
2021-11-24 23:44:49 +0900

    Fix typo

--- 18-interrupts/cpu/isr.h
+++ 18-interrupts/cpu/isr.h
@@ -67,6 +67,6 @@ void isr_install();
 void isr_handler(registers_t r);
 
 typedef void (*isr_t)(registers_t);
-void register_interrupt_handler(u8 n, isr_h handler);
+void register_interrupt_handler(u8 n, isr_t handler);
 
 #endif
