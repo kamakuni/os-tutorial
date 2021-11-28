99ece5972ffe7bfd674a485073042fcee1110669
sugawara kensuke <superradio86@gmail.com>
2021-11-28 22:51:04 +0900

    Add reverse and strlen function

--- 20-interrupts-timer/kernel/util.c
+++ 20-interrupts-timer/kernel/util.c
@@ -1,8 +1,15 @@
+#include "util.h"
+
 void memory_copy(char *source, char *dest, int nbytes) {
     int i;
     for (i = 0; i < nbytes; i++) {
         *(dest + i) = *(source + i);
     }
+}
+
+void memory_set(u8 *dest, u8 val, u32 len) {
+    u8 *temp = (u8 *)dest;
+    for (; len != 0; len--) *temp++ = val;
 }
 
 /**
@@ -20,4 +27,21 @@ void int_to_ascii(int n, char str[]) {
     str[i] = '\0';
 
     /* TODO: implememt "reverse" */
+}
+
+/* K&R */
+void reverse(char s[]) {
+    int c, i, j;
+    for (i = 0; j = strlen(s)-1; i < j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
+
+/* K&R */
+int strlen(char s[]) {
+    int i =0;
+    while (s[i] != '\0') ++i;
+    return i;
 }
