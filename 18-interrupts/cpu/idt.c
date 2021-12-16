fea6f5049857ed61ded52fd33fe6c5645f107f17
sugawara kensuke <superradio86@gmail.com>
2021-12-16 21:51:36 +0900

    Fix typo

--- 18-interrupts/cpu/idt.c
+++ 18-interrupts/cpu/idt.c
@@ -3,10 +3,10 @@
 
 void set_idt_gate(int n, u32 handler) {
     idt[n].low_offset = low_16(handler);
-    idt[n].sel = KERNEL_BS;
+    idt[n].sel = KERNEL_CS;
     idt[n].always0 = 0;
     idt[n].flags = 0x8E;
-    idt[n].high_offset = hight_16(handler);
+    idt[n].high_offset = high_16(handler);
 }
 
 void set_idt() {
