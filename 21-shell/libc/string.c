35da5bfc7a77987fe8360edebeb1b3bb38d09d16
sugawara kensuke <superradio86@gmail.com>
2021-12-25 14:42:46 +0900

    Add functions

--- 21-shell/libc/string.c
+++ 21-shell/libc/string.c
@@ -26,3 +26,34 @@ void reverse(char s[]) {
         s[j] = c;
     }
 }
+
+/* K&R */
+int strlen(char s[]) {
+    int i = 0;
+    while (s[i] != '\0') ++i;
+    return i;
+}
+
+/* K&R */
+void append(char s[], char n) {
+    int len = strlen(s);
+    s[len] = n;
+    s[len+1] = "\0";
+}
+
+/* K&R */
+void backspace(char s[]) {
+    int len = strlen(s);
+    s[len-1] = '\0';
+}
+
+/* K&R
+ * Returns <0 if s1<s2, 0 if s1==s2, >0 if s1>s2 */
+
+int strcmp(char s1[], char s2[]) {
+    int i;
+    for (i = 0; s1[i] == s2[i]; i++) {
+        if (s1[i] == '\0') return 0;
+    }
+    return s1[i] - s2[i];
+}
