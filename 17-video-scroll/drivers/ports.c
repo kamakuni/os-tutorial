d143379151bb8edf27d85a651a04004f2444a37a
sugawara kensuke <superradio86@gmail.com>
2022-01-04 23:21:05 +0900

    update ports.c

--- 17-video-scroll/drivers/ports.c
+++ 17-video-scroll/drivers/ports.c
@@ -1,7 +1,7 @@
 /**
  * Read a byte from the specified port
  */
-unsigned char port_byte_in (u16 port) {
+unsigned char port_byte_in (unsigned short port) {
     unsigned char result;
     /* Inline assembler syntax
      * !! Notice how the source and destination registers are switched from NASM !!
@@ -14,7 +14,7 @@ unsigned char port_byte_in (u16 port) {
     return result;
 }
 
-void port_byte_out (u16 port, u8 data) {
+void port_byte_out (unsigned short port, unsigned char data) {
     /* Notice how here both registers are mapped to C variables and
      * nothing is returned, thus, no equals '=' in the asm syntax
      * However we see a come since there are two variables in the input area
@@ -23,12 +23,12 @@ void port_byte_out (u16 port, u8 data) {
     __asm__("out %%al, %%dx" : : "a" (data), "d" (port));
 }
 
-unsigned short port_word_in (u16 port) {
+unsigned short port_word_in (unsigned short port) {
     unsigned short result;
     __asm__("in %%dx, %%ax" : "=a" (result) : "d" (port));
     return result;
 }
 
-void port_word_out (u16 port, u16 data) {
+void port_word_out (unsigned short port, unsigned short data) {
     __asm__("out %%ax, %%dx" : : "a" (data), "d" (port));
 }
