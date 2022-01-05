09f29fdb21a7df219de4a57297c8d064a75dceae
sugawara kensuke <superradio86@gmail.com>
2022-01-05 23:17:12 +0900

    fix isr.h

--- 18-interrupts/cpu/isr.h
+++ 18-interrupts/cpu/isr.h
@@ -58,9 +58,9 @@ extern void isr31();
 typedef struct
 {
     u32 ds;                                /* Data segment selector */
-    u32 edi, esi, ebp, esp, ebx, ecx, eax; /* Pushed by pusha. */
+    u32 edi, esi, ebp, esp, ebx, edx, ecx, eax; /* Pushed by pusha. */
     u32 int_no, err_code;                  /* Interrupts number and error code (if applicable) */
-    u32 epi, cs, eflags, useresp, ss;      /* Pushed by the processor automatically */
+    u32 eip, cs, eflags, useresp, ss;      /* Pushed by the processor automatically */
 } registers_t;
 
 void isr_install();
