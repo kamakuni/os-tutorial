b1291fabe61c684d5c51e8ca8ac1e37681760033
sugawara kensuke <superradio86@gmail.com>
2022-01-06 21:49:56 +0900

    fix isr.h

--- 18-interrupts/cpu/isr.h
+++ 18-interrupts/cpu/isr.h
@@ -37,23 +37,6 @@ extern void isr29();
 extern void isr30();
 extern void isr31();
 
-#define IRQ0 32
-#define IRQ1 33
-#define IRQ2 34
-#define IRQ3 35
-#define IRQ4 36
-#define IRQ5 37
-#define IRQ6 38
-#define IRQ7 39
-#define IRQ8 40
-#define IRQ9 41
-#define IRQ10 42
-#define IRQ11 43
-#define IRQ12 44
-#define IRQ13 45
-#define IRQ14 46
-#define IRQ15 47
-
 /* Struct which aggregates many registers */
 typedef struct
 {
