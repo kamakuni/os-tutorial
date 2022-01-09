1ae0c7d1e2fcc443f9057d7ef53ab5a64476c186
sugawara kensuke <superradio86@gmail.com>
2022-01-09 23:27:57 +0900

    add memory_set function

--- 18-interrupts/kernel/util.c
+++ 18-interrupts/kernel/util.c
@@ -5,6 +5,11 @@ void memory_copy(char *source, char *dest, int nbytes) {
     }
 }
 
+void memory_set(u8 *dest, u8 val, u32 len) {
+    u8 *temp = (u8 *)dest;
+    for ( ; len != 0; len--) *temp++ = val;
+}
+
 /**
  * K&R implementation
  */
--- 18-interrupts/kernel/util.h
+++ 18-interrupts/kernel/util.h
@@ -1,2 +1,5 @@
+#include "../cpu/types.h"
+
 void memory_copy(char *source, char *dest, int nbytes);
+void moery_set(u8 *dest, u8 val, u32 len);
 void int_to_ascii(int n, char str[]);
