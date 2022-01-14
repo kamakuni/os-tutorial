14cea86937d7e65e7f06342c51e93caa44abfc4e
sugawara kensuke <superradio86@gmail.com>
2022-01-14 23:42:20 +0900

    update isr.c

--- 19-interrupts-irqs/cpu/isr.c
+++ 19-interrupts-irqs/cpu/isr.c
@@ -90,3 +90,20 @@ void isr_handler(registers_t r) {
     kprint(exception_messages[r.int_no]);
     kprint("\n");
 }
+
+void register_interrupt_handler(u8 n, isr_t handler) {
+    interrupt_handlers[n] = handler;
+}
+
+void irq_handler(registers_t r) {
+    /* After every interrupot we need to send an EOI to the PICs
+     * or they will not send another interrupt again */
+     if (r.int_no >= 40) port_byte_out(0xA0, 0x20); /* slave */
+     port_byte_out(0x20, 0x20);
+
+     /* Handle the interrupt in a more modular way */
+     if (interrupt_handlers[r.int_no] != 0) {
+         isr_t handler = interrupt_handlers[r.int_no];
+         handler(r);
+     }
+}
