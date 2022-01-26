3042784486bb9b28a4b81eb007c4397d66c0a082
sugawara kensuke <superradio86@gmail.com>
2022-01-26 20:39:45 +0900

    Update util.h

--- 20-interrupts-timer/kernel/util.h
+++ 20-interrupts-timer/kernel/util.h
@@ -3,3 +3,5 @@
 void memory_copy(char *source, char *dest, int nbytes);
 void moery_set(u8 *dest, u8 val, u32 len);
 void int_to_ascii(int n, char str[]);
+void reverse(char s[]);
+int strlen(char s[]);
