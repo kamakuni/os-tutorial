ac7bdaa67435c82a2a3bed76c9c5053a31b5a611
sugawara kensuke <superradio86@gmail.com>
2022-02-05 23:31:48 +0900

    Update idt.h

--- 19-interrupts-irqs/cpu/idt.h
+++ 19-interrupts-irqs/cpu/idt.h
@@ -1,3 +1,4 @@
+#ifndef IDT_H
 #define IDT_H
 
 #include "types.h"
@@ -33,3 +34,5 @@ idt_register_t idt_reg;
 /* Function implemented int idt.c */
 void set_idt_gate(int n, u32 handler);
 void set_idt();
+
+#endif
