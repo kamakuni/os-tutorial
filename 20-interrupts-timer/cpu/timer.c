6e54f4eec8533dab94735af7db2eec73f076baa4
sugawara kensuke <superradio86@gmail.com>
2022-02-11 22:55:55 +0900

    Fix timer.c

--- 20-interrupts-timer/cpu/timer.c
+++ 20-interrupts-timer/cpu/timer.c
@@ -21,7 +21,7 @@ void init_timer(u32 freq) {
 
     /* Get the PIT value: hardware clock at 1193180 Hz */
     u32 divisor = 1193180 / freq;
-    u8 low = (u8)(divisor & oxFF);
+    u8 low = (u8)(divisor & 0xFF);
     u8 high = (u8)( (divisor >> 8) & 0xFF);
     /* Send the command */
     port_byte_out(0x43, 0x36); /* Command port */
