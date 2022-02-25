f20cb9d132fd5f79960147382a7c6fd19b084f84
sugawara kensuke <superradio86@gmail.com>
2022-02-25 22:00:18 +0900

    Fix typo

--- 21-shell/cpu/idt.c
+++ 21-shell/cpu/idt.c
@@ -1,9 +1,8 @@
 #include "idt.h"
-#include "../kernel/util.h"
 
 void set_idt_gate(int n, u32 handler) {
     idt[n].low_offset = low_16(handler);
-    idt[n].sel = KERNEL_BS;
+    idt[n].sel = KERNEL_CS;
     idt[n].always0 = 0;
     idt[n].flags = 0x8E;
     idt[n].high_offset = hight_16(handler);
