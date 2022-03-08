1248d744548ec79e7a6ca81a700092089b7a01c1
sugawara kensuke <superradio86@gmail.com>
2022-03-08 20:17:57 +0900

    Fix print_char function

--- 21-shell/drivers/screen.c
+++ 21-shell/drivers/screen.c
@@ -81,6 +81,9 @@ int print_char(char c, int col, int row,char attr) {
     if (c == '\n') {
         row = get_offset_row(offset);
         offset = get_offset(0, row+1);
+    } else if (c == 0x08) { /* Backspace */
+        vidmem[offset] = ' ';
+        vidmem[offset+1] = attr;
     } else {
         vidmem[offset] = c;
         vidmem[offset+1] = attr;
