6b3dc0108a4c708cbd1ca127f6da6c92d137978f
sugawara kensuke <superradio86@gmail.com>
2022-03-19 15:14:04 +0900

    Add code to test kmalloc

--- 22-malloc/kernel/kernel.c
+++ 22-malloc/kernel/kernel.c
@@ -20,6 +20,15 @@ void user_input(char *input) {
         /* Lesson 22: Code to test kmalloc, the rest is unchanged */
         u32 phys_addr;
         u32 page = kmalloc(1000, 1, &phys_addr);
+        char page_str[16] = "";
+        hex_to_ascii(page, page_str);
+        char phys_str = "";
+        hex_to_ascii(phys_addr, phys_str);
+        kprint("Page: ");
+        kprint(page_str);
+        kprint(", physical address");
+        kprint(phys_str);
+        kprint("\n");
     }
     kprint("You said: ");
     kprint(input);
