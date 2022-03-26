e36ea3e78ad0596fee6ab763a8feafe66f50349c
sugawara kensuke <superradio86@gmail.com>
2022-03-26 17:16:17 +0900

    Update type.h

--- 23-fixes/cpu/types.h
+++ 23-fixes/cpu/types.h
@@ -1,13 +1,6 @@
 #define TYPES_H
 
-/* Instead of using 'chars' to allcate non-character byte,
- * we will use these new type with no semantic meaning */
-typedef unsigned int u32;
-typedef int s32;
-typedef unsigned short u16;
-typedef short s16;
-typedef unsigned char u8;
-typedef char s8;
+#include <stdint.h>
 
-#define low_16(address) (u16)((address)&0xFFFF)
-#define high_16(address) (u16)(((address) >> 16) & 0xFFFF)
+#define low_16(address) (uint16_t)((address)&0xFFFF)
+#define high_16(address) (uint16_t)(((address) >> 16) & 0xFFFF)
