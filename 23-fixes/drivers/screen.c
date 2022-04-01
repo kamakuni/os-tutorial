844e62db51bbbc420e166ab9a5b050cb09e71c1d
sugawara kensuke <superradio86@gmail.com>
2022-04-01 23:44:22 +0900

    Update screen.c

--- 23-fixes/drivers/screen.c
+++ 23-fixes/drivers/screen.c
@@ -2,6 +2,8 @@
 #include "../cpu/ports.h"
 #include "../libc/mem.h"
 
+#include <stdint.h>
+
 /* Declaration of private functions */
 int get_cursor_offset();
 void set_cursor_offset(int offset);
@@ -64,7 +66,7 @@ void kprint_backspace() {
  * Sets the video cursor to the returned offset
  **/
 int print_char(char c, int col, int row,char attr) {
-    u8 *vidmem = (u8*) VIDEO_ADDRESS;
+    uint8_t *vidmem = (uint8_t*) VIDEO_ADDRESS;
     if (!attr) attr = WHITE_ON_BLACK;
 
     /* Error control: print a red 'E' if the coords aren't right */
@@ -93,12 +95,12 @@ int print_char(char c, int col, int row,char attr) {
     if (offset >= MAX_ROWS * MAX_COLS *2) {
         int i;
         for (i =0; i < MAX_ROWS; i++)
-            memory_copy((u8*)(get_offset(0, i) + VIDEO_ADDRESS),
-                        (u8*)(get_offset(0, i-1) + VIDEO_ADDRESS),
+            memory_copy((uint8_t*)(get_offset(0, i) + VIDEO_ADDRESS),
+                        (uint8_t*)(get_offset(0, i-1) + VIDEO_ADDRESS),
                         MAX_COLS * 2);
 
         /* Blank last line */
-        char *last_line = (char *) (get_offset(0, MAX_ROWS-1) + (u8*) VIDEO_ADDRESS);
+        char *last_line = (char *) (get_offset(0, MAX_ROWS-1) + (uint8_t*) VIDEO_ADDRESS);
         for (i = 0; i < MAX_COLS * 2; i++) last_line[i] = 0;
 
         offset -= 2 * MAX_COLS;
@@ -132,7 +134,7 @@ void set_cursor_offset(int offset) {
 void clear_screen() {
     int screen_size = MAX_COLS * MAX_ROWS;
     int i;
-    u8 *screen = (u8*) VIDEO_ADDRESS;
+    uint8_t *screen = (uint8_t*) VIDEO_ADDRESS;
 
     for (i = 0; i < screen_size; i++) {
         screen[i*2] = ' ';
